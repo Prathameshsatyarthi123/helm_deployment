acda76f1fa76a276d15f0ef85533c3e43f0279b0
root <root@ip-172-31-84-255.ec2.internal>
2025-09-05 06:57:00 +0000

    added metric_svc

---
+++ metric_svc/metrics_service.py
@@ -0,0 +1,16 @@
+# metrics_service.py
+from fastapi import FastAPI
+import psutil
+
+app = FastAPI()
+
+@app.get("/metrics")
+def get_metrics():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    memory = psutil.virtual_memory()
+    return {
+        "cpu_percent": cpu_usage,
+        "memory_percent": memory.percent,
+        "total_memory_gb": round(memory.total / (1024 ** 3), 2),
+        "used_memory_gb": round(memory.used / (1024 ** 3), 2)
+    }
