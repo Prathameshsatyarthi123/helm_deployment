496eeb9b514692d96094f02db4ab90cda656932f
Prathamesh Satyarthi <80326827+Prathameshsatyarthi123@users.noreply.github.com>
2025-09-11 20:26:57 +0530

    Update metrics_service.py

--- metric_svc/metrics_service.py
+++ metric_svc/metrics_service.py
@@ -15,7 +15,7 @@ def get_metrics():
     cpu_usage = psutil.cpu_percent(interval=1)
     memory = psutil.virtual_memory()
     return {
-        "cpu_percent": cpu_usage,
+        "cpu_percentage": cpu_usage,
         "memory_percent": memory.percent,
         "total_memory_gb": round(memory.total / (1024 ** 3), 2),
         "used_memory_gb": round(memory.used / (1024 ** 3), 2),
